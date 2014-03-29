b2e777e2b2811a6191cd227b666846b8e7cf358a
Aistis <aistis8624@gmail.com>
2014-03-30 00:47:18 +0200

    Added all filenames symbol counts output

--- FileStats.py
+++ FileStats.py
@@ -1,3 +1,5 @@
+all_symbols = {}
+
 # Stores file information
 class FileStats(object):
 
@@ -8,15 +10,31 @@ class FileStats(object):
 
     def analyze_name(self):
         for char in self.file_name:
+            # for a file
             try:
                 self.symbols[ord(char)] += 1
             except KeyError:
                 self.symbols[ord(char)] = 1
 
+            # for all files together
+            try:
+                all_symbols[ord(char)] += 1
+            except KeyError:
+                all_symbols[ord(char)] = 1
+
     def get_counted_symbols(self):
         result = self.file_name
 
         for symbol, times in self.symbols.items():
+            result += '\n\t' + chr(symbol) + ' ' + str(times)
+
+        return result
+
+    @staticmethod
+    def get_all_counted_symbols():
+        result = 'All symbols in all files'
+
+        for symbol, times in all_symbols.items():
             result += '\n\t' + chr(symbol) + ' ' + str(times)
 
         return result
--- StatsCounting.py
+++ StatsCounting.py
@@ -7,16 +7,14 @@ directory = sys.argv[1]
 
 files = [f for f in listdir(directory) if isfile(join(directory, f))]
 
-all_files_names = ''
-
-for i, file in enumerate(files):
-    all_files_names += ' ' + file
-    files[i] = FileStats.FileStats(file)
+files_stats = [FileStats.FileStats(file) for file in files]
 
 string_to_file = ''
 
-for file in files:
+for file in files_stats:
     string_to_file += file.get_counted_symbols() + '\n\n'
+
+string_to_file = FileStats.FileStats.get_all_counted_symbols() + '\n\n' + string_to_file
 
 with open(sys.argv[2], 'w+') as output:
     output.write(string_to_file)
